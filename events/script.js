7f913d68f0669e480df4559b1237543fb50af9fd
Naman Shah <nashah2001@gmail.com>
2020-10-01 05:36:21 +0530

    events page almost done

---
+++ events/script.js
@@ -0,0 +1,34 @@
+$(".technical-section").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".cards").css({"display": "none"});
+	$(".technical-section").css({"display": "grid"});
+});
+
+$(".ideate-section").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".cards").css({"display": "none"});
+	$(".ideate-section").css({"display": "grid"});
+});
+
+$(".informals-section").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".cards").css({"display": "none"});
+	$(".informals-section").css({"display": "grid"});
+});
+
+$(".shield-section").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".cards").css({"display": "none"});
+	$(".shield-section").css({"display": "grid"});
+});
+
+$(".preevents-section").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".cards").css({"display": "none"});
+	$(".preevents-section").css({"display": "grid"});
+});
