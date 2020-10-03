04faf9d4eca95132cabc74920cdbda57800bd4c4
manasivaraiya <m.varaiya@somaiya.edu>
2020-10-03 11:23:40 +0530

    buttons and containers made in sponsors section

--- events/script.js
+++ events/script.js
@@ -32,3 +32,33 @@ $(".preevents-section").click(function(){
 	$(".cards").css({"display": "none"});
 	$(".preevents-section").css({"display": "grid"});
 });
+
+
+// sponsor section
+$(".sec1").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".section").css({"display": "none"});
+	$(".sec1").css({"display": "grid"});
+});
+
+$(".sec2").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".section").css({"display": "none"});
+	$(".sec2").css({"display": "grid"});
+});
+
+$(".sec3").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".section").css({"display": "none"});
+	$(".sec3").css({"display": "grid"});
+});
+
+$(".sec4").click(function(){
+	$("button").css({"color": "white", "background-color": "transparent", "border-color": "white"})
+	$(this).css({"color": "black", "background-color": "white", "border-color": "black"})
+	$(".section").css({"display": "none"});
+	$(".sec4").css({"display": "grid"});
+});
