0bfe66c365b0e3a6eff303a015415b8a68cd858b
Naman Shah <nashah2001@gmail.com>
2020-10-05 23:08:48 +0530

    event page button functionality done

--- events/script.js
+++ events/script.js
@@ -1,122 +1,73 @@
-// $(".technical-btn").click(function () {
-//   $("button").css({
-//     color: "white",
-//     "background-color": "transparent",
-//     "border-color": "white",
-//   });
-//   $(this).css({
-//     color: "black",
-//     "background-color": "white",
-//     "border-color": "black",
-//   });
-//   $(".cards").css({ display: "none" });
-//   $(".technical-section").css({ display: "grid" });
-// });
-
-// $(".ideate-btn").click(function () {
-//   $("button").css({
-//     color: "white",
-//     "background-color": "transparent",
-//     "border-color": "white",
-//   });
-//   $(this).css({
-//     color: "black",
-//     "background-color": "white",
-//     "border-color": "black",
-//   });
-//   $(".cards").css({ display: "none" });
-//   $(".ideate-section").css({ display: "grid" });
-// });
-
-// $(".informals-btn").click(function () {
-//   $("button").css({
-//     color: "white",
-//     "background-color": "transparent",
-//     "border-color": "white",
-//   });
-//   $(this).css({
-//     color: "black",
-//     "background-color": "white",
-//     "border-color": "black",
-//   });
-//   $(".cards").css({ display: "none" });
-//   $(".informals-section").css({ display: "grid" });
-// });
-
-// $(".shield-btn").click(function () {
-//   $("button").css({
-//     color: "white",
-//     "background-color": "transparent",
-//     "border-color": "white",
-//   });
-//   $(this).css({
-//     color: "black",
-//     "background-color": "white",
-//     "border-color": "black",
-//   });
-//   $(".cards").css({ display: "none" });
-//   $(".shield-section").css({ display: "grid" });
-// });
-
-// $(".preevents-btn").click(function () {
-//   $("button").css({
-//     color: "white",
-//     "background-color": "transparent",
-//     "border-color": "white",
-//   });
-//   $(this).css({
-//     color: "black",
-//     "background-color": "white",
-//     "border-color": "black",
-//   });
-//   $(".cards").css({ display: "none" });
-//   $(".preevents-section").css({ display: "grid" });
-// });
-
-// default
-$(".technical-btn").css({
-  backgroundColor: "white",
-  color: "black",
-});
-// $(".cards").css({ display: "none" });
-// $(".technical-section").css({ display: "grid" });
-
 $('.technical-btn').click(function(){
-  $(".ideate-section").addClass("hidden");
-  $(".informals-section").addClass("hidden");
-  $(".shield-section").addClass("hidden");
-  $(".preevents-section").addClass("hidden");
-  $(".technical-section").toggleClass("hidden");
+	$(".ideate-btn").removeClass("clicked-btn");
+	$(".informals-btn").removeClass("clicked-btn");
+	$(".shield-btn").removeClass("clicked-btn");
+	$(".preevents-btn").removeClass("clicked-btn");
+	$(".technical-btn").addClass("clicked-btn");
+	// cards
+	$(".ideate-section").addClass("hidden");
+	$(".informals-section").addClass("hidden");
+	$(".shield-section").addClass("hidden");
+	$(".preevents-section").addClass("hidden");
+	$(".technical-section").toggleClass("hidden");
 })
 
 $('.ideate-btn').click(function(){
-  $(".technical-section").addClass("hidden");
-  $(".informals-section").addClass("hidden");
-  $(".shield-section").addClass("hidden");
-  $(".preevents-section").addClass("hidden");
-  $(".ideate-section").toggleClass("hidden");
+	$(".technical-btn").removeClass("clicked-btn");
+	$(".informals-btn").removeClass("clicked-btn");
+	$(".shield-btn").removeClass("clicked-btn");
+	$(".preevents-btn").removeClass("clicked-btn");
+	$(".ideate-btn").addClass("clicked-btn");
+	// cards
+	$(".technical-section").addClass("hidden");
+	$(".informals-section").addClass("hidden");
+	$(".shield-section").addClass("hidden");
+	$(".preevents-section").addClass("hidden");
+	$(".ideate-section").toggleClass("hidden");
 })
 
 $('.informals-btn').click(function(){
-  $(".technical-section").addClass("hidden");
-  $(".ideate-section").addClass("hidden");
-  $(".shield-section").addClass("hidden");
-  $(".preevents-section").addClass("hidden");
-  $(".informals-section").toggleClass("hidden");
+	$(".technical-btn").removeClass("clicked-btn");
+	$(".ideate-btn").removeClass("clicked-btn");
+	$(".shield-btn").removeClass("clicked-btn");
+	$(".preevents-btn").removeClass("clicked-btn");
+	$(".informals-btn").addClass("clicked-btn");
+	// cards
+	$(".technical-section").addClass("hidden");
+	$(".ideate-section").addClass("hidden");
+	$(".shield-section").addClass("hidden");
+	$(".preevents-section").addClass("hidden");
+	$(".informals-section").toggleClass("hidden");
 })
 
 $('.shield-btn').click(function(){
-  $(".technical-section").addClass("hidden");
-  $(".ideate-section").addClass("hidden");
-  $(".informals-section").addClass("hidden");
-  $(".preevents-section").addClass("hidden");
-  $(".shield-section").toggleClass("hidden");
+	$(".technical-btn").removeClass("clicked-btn");
+	$(".ideate-btn").removeClass("clicked-btn");
+	$(".informals-btn").removeClass("clicked-btn");
+	$(".preevents-btn").removeClass("clicked-btn");
+	$(".shield-btn").addClass("clicked-btn");
+	// cards
+	$(".technical-section").addClass("hidden");
+	$(".ideate-section").addClass("hidden");
+	$(".informals-section").addClass("hidden");
+	$(".preevents-section").addClass("hidden");
+	$(".shield-section").toggleClass("hidden");
 })
 
 $('.preevents-btn').click(function(){
-  $(".technical-section").addClass("hidden");
-  $(".ideate-section").addClass("hidden");
-  $(".informals-section").addClass("hidden");
-  $(".shield-section").addClass("hidden");
-  $(".preevents-section").toggleClass("hidden");
+	$(".technical-btn").removeClass("clicked-btn");
+	$(".ideate-btn").removeClass("clicked-btn");
+	$(".informals-btn").removeClass("clicked-btn");
+	$(".shield-btn").removeClass("clicked-btn");
+	$(".preevents-btn").addClass("clicked-btn");
+	// cards
+	$(".technical-section").addClass("hidden");
+	$(".ideate-section").addClass("hidden");
+	$(".informals-section").addClass("hidden");
+	$(".shield-section").addClass("hidden");
+	$(".preevents-section").toggleClass("hidden");
 })
+
+if (window.innerWidth < 768){
+	$(".technical-section").addClass("hidden");
+}
