bf266c29280a2a16afa405d5e149f5ca8b1def93
twilight-warlock <devansh.ts@somaiya.edu>
2020-10-06 18:07:26 +0530

    Basic preloader added

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-var display = 1;
-$("#sponsor_2020").show();
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#sponsor_2020").focus(function(){
-      $(".first").css("display", "block");
-    });
-  });
-});
+// var display = 1;
+// $("#sponsor_2020").show();
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $("#sponsor_2020").focus(function(){
+//       $(".first").css("display", "block");
+//     });
+//   });
+// });
 
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#sponsor_2019").focus(function(){
-      $(".second").css("display", "block");
-    });
-  });
-});
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $("#sponsor_2019").focus(function(){
+//       $(".second").css("display", "block");
+//     });
+//   });
+// });
 
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#sponsor_2018").focus(function(){
-      $(".third").css("display", "block");
-    });
-  });
-});
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $("#sponsor_2018").focus(function(){
+//       $(".third").css("display", "block");
+//     });
+//   });
+// });
 
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#sponsor_2017").focus(function(){
-      $(".fourth").css("display", "block");
-    });
-  });
-});
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $("#sponsor_2017").focus(function(){
+//       $(".fourth").css("display", "block");
+//     });
+//   });
+// });
+let pageLoad = false;
+setTimeout(() => {
+  if (!pageLoad) {
+    $(".preloader-anim").addClass("hidden");
+    pageLoad = true;
+  }
+}, 5000);
