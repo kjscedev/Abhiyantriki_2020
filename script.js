bc2242b0fe95dfe46fc5a7ad94bc8d0f91a53dd4
twilight-warlock <devansh.ts@somaiya.edu>
2020-10-10 21:18:46 +0530

    Added logos to all pages, made home resposive and created a new folder

--- script.js
+++ script.js
@@ -1,45 +1,6 @@
-// var display = 1;
-// $("#sponsor_2020").show();
-// $(document).ready(function(){
-//   $("button").click(function(){
-//     $("#sponsor_2020").focus(function(){
-//       $(".first").css("display", "block");
-//     });
-//   });
-// });
-
-// $(document).ready(function(){
-//   $("button").click(function(){
-//     $("#sponsor_2019").focus(function(){
-//       $(".second").css("display", "block");
-//     });
-//   });
-// });
-
-// $(document).ready(function(){
-//   $("button").click(function(){
-//     $("#sponsor_2018").focus(function(){
-//       $(".third").css("display", "block");
-//     });
-//   });
-// });
-
-// $(document).ready(function(){
-//   $("button").click(function(){
-//     $("#sponsor_2017").focus(function(){
-//       $(".fourth").css("display", "block");
-//     });
-//   });
-// });
-let pageLoad = false;
 setTimeout(() => {
-  if (!pageLoad) {
     $(".preloader-anim").addClass("hidden");
-    // $(".outer").removeClass("hidden");
     $(".outer").removeClass('hidden');
-
-    pageLoad = true;
-  }
 }, 5000);
 
 // Carousel
