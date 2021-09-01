48bed776f37ad5207232674b0283569c761eb79b
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-09-01 20:50:31 +0530

    removed unwanted csss selectors, slightly modified sizing

--- js/page.js
+++ js/page.js
@@ -4,8 +4,6 @@ $(".scroll").click(function (e) {
   e.preventDefault();
   var target = $(this).attr("href");
 
-//   console.log(target);
-
   if (target.length < 2) return;
 
   var element = document.querySelector(target);
