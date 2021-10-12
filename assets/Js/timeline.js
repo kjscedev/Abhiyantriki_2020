6b705b01a82825d128ffbbfca5913ed38de115d1
jaymalave <jaym.github@gmail.com>
2021-10-12 12:05:24 +0530

    timeline functionality added

---
+++ assets/Js/timeline.js
@@ -0,0 +1,16 @@
+function change17(){
+    document.getElementById('changedText').innerHTML = 'Text for 2017';
+    document.getElementById('changedImage').src = '/assets/images/timeline/2017.png';
+}
+function change18(){
+    document.getElementById('changedText').innerHTML = 'Text for 2018';
+    document.getElementById('changedImage').src = '/assets/images/timeline/2018.png';
+}
+function change19(){
+    document.getElementById('changedText').innerHTML = 'Text for 2019';
+    document.getElementById('changedImage').src = '/assets/images/timeline/2019.png';
+}
+function change20(){
+    document.getElementById('changedText').innerHTML = 'Text for 2020';
+    document.getElementById('changedImage').src = '/assets/images/timeline/1998.png';
+}
