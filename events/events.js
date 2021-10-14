55a682106a2cd6b5a63b7c582dbb4311d9f44395
jaymalave <jaym.github@gmail.com>
2021-10-14 11:47:38 +0530

    added js for event names toggle

--- events/events.js
+++ events/events.js
@@ -1,21 +1,62 @@
+//to do: update images
+
 function changeToTechnical() {
   console.log("update DOM elements for technical");
+  document.getElementById("name-1").innerHTML = "Crackathon";
+  document.getElementById("name-2").innerHTML = "Codespree";
+  document.getElementById("name-3").innerHTML = "tech event 3";
+  document.getElementById("name-4").innerHTML = "tech event 4";
+  document.getElementById("name-5").innerHTML = "tech event 5";
+  document.getElementById("name-6").innerHTML = "tech event 6";
+  document.getElementById("name-7").innerHTML = "tech event 7";
+  document.getElementById("name-8").innerHTML = "tech event 8";
 }
 
 function changeToShield() {
   console.log("update DOM elements for shield");
+  document.getElementById("name-1").innerHTML = "shield event 1";
+  document.getElementById("name-2").innerHTML = "shield event 2";
+  document.getElementById("name-3").innerHTML = "shield event 3";
+  document.getElementById("name-4").innerHTML = "shield event 4";
+  document.getElementById("name-5").innerHTML = "shield event 5";
+  document.getElementById("name-6").innerHTML = "shield event 6";
+  document.getElementById("name-7").innerHTML = "shield event 7";
+  document.getElementById("name-8").innerHTML = "shield event 8";
+   
 }
 
 function changeToInformals() {
   console.log("update DOM elements for informals");
+  document.getElementById("name-1").innerHTML = "informals event 1";
+  document.getElementById("name-2").innerHTML = "informals event 2";
+  document.getElementById("name-3").innerHTML = "informals event 3";
+  document.getElementById("name-4").innerHTML = "informals event 4";
+  document.getElementById("name-5").innerHTML = "informals event 5";
+  document.getElementById("name-6").innerHTML = "informals event 6";
+  document.getElementById("name-7").innerHTML = "informals event 7";
+  document.getElementById("name-8").innerHTML = "informals event 8";
 }
 
 function changeToIdeate() {
   console.log("update DOM elements for ideate");
+  document.getElementById("name-1").innerHTML = "ideate event 1";
+  document.getElementById("name-2").innerHTML = "ideate event 2";
+  document.getElementById("name-3").innerHTML = "ideate event 3";
+  document.getElementById("name-4").innerHTML = "ideate event 4";
+  document.getElementById("name-5").innerHTML = "ideate event 5";
+  document.getElementById("name-6").innerHTML = "ideate event 6";
+  document.getElementById("name-7").innerHTML = "ideate event 7";
+  document.getElementById("name-8").innerHTML = "ideate event 8";
 }
 
 function changeToPreevents() {
   console.log("update DOM elements for pre-events");
+  document.getElementById("name-1").innerHTML = "pre-event event 1";
+  document.getElementById("name-2").innerHTML = "pre-event event 2";
+  document.getElementById("name-3").innerHTML = "pre-event event 3";
+  document.getElementById("name-4").innerHTML = "pre-event event 4";
+  document.getElementById("name-5").innerHTML = "pre-event event 5";
+  document.getElementById("name-6").innerHTML = "pre-event event 6";
+  document.getElementById("name-7").innerHTML = "pre-event event 7";
+  document.getElementById("name-8").innerHTML = "pre-event event 8";
 }
-
-
