1b141594d4df79cd9782f697a3012caf2e0bcc79
jaymalave <jaym.github@gmail.com>
2021-10-18 12:44:09 +0530

    added timeline content

--- assets/Js/timeline.js
+++ assets/Js/timeline.js
@@ -1,16 +1,52 @@
-function change17(){
-    document.getElementById('changedText').innerHTML = 'Text for 2017';
-    document.getElementById('changedImage').src = '/assets/images/timeline/2017.png';
+function change98() {
+  document.getElementById("changedText").innerHTML =
+    "Abhiyantriki introduced as an individual, standalone festival of technical grandeur and pomp.";
+  document.getElementById("changedImage").src =
+    "/assets/images/timeline/1998.png";
 }
-function change18(){
-    document.getElementById('changedText').innerHTML = 'Text for 2018';
-    document.getElementById('changedImage').src = '/assets/images/timeline/2018.png';
+
+function change08() {
+  document.getElementById("changedText").innerHTML =
+    "Funds collected this year were all donated for the rehabilitation of the flood struck Bihar and Abhiyantriki rose in popularity with extensive media coverage.";
+  document.getElementById("changedImage").src =
+    "/assets/images/timeline/2008.png";
 }
-function change19(){
-    document.getElementById('changedText').innerHTML = 'Text for 2019';
-    document.getElementById('changedImage').src = '/assets/images/timeline/2019.png';
+
+function change09() {
+  document.getElementById("changedText").innerHTML =
+    "KJSCE marks 25 years since its inauguration,  and the closing ceremony of Abhiyantriki was ornamented by the presence of late Dr. A.P.J. Abdul Kalam.";
+  document.getElementById("changedImage").src =
+    "/assets/images/timeline/2009.png";
 }
-function change20(){
-    document.getElementById('changedText').innerHTML = 'Text for 2020';
-    document.getElementById('changedImage').src = '/assets/images/timeline/1998.png';
+
+function change14() {
+  document.getElementById("changedText").innerHTML =
+    "The maiden year for Start-up and Auto Expo, Abhiyantriki this year also encompassed Rohit Suri, the Jagaur India CEO as one of the keynote speakers.";
+  document.getElementById("changedImage").src =
+    "/assets/images/timeline/2014.png";
+}
+function change16() {
+  document.getElementById("changedText").innerHTML =
+    "Abhiyantriki extended by a day, now was a two-day long fest with its flagship event being a technical exhibition presided over by ISRO.";
+  document.getElementById("changedImage").src =
+    "/assets/images/timeline/2016.png";
+}
+
+function change17() {
+  document.getElementById("changedText").innerHTML =
+    "Attending to the likes of DRDO, DAE and BARC, Abhiyantriki gamered a running footfall of over 15,000 for the first time in its history!";
+  document.getElementById("changedImage").src =
+    "/assets/images/timeline/2017.png";
+}
+function change18() {
+  document.getElementById("changedText").innerHTML =
+    "Abhiyantriki had the Indian Army exhibiting their artillery as the highlight, along with prominent personalities like Dr S. Christopher and Mr. Vilas Shinde gracing the occasion.";
+  document.getElementById("changedImage").src =
+    "/assets/images/timeline/2018.png";
+}
+function change19() {
+  document.getElementById("changedText").innerHTML =
+    "This year witnessed the Indian Navy rolling out an array of Naval armaments at Abhiyantriki along with a riveting talk on the Big Bang by Dr. Bikash Chandra Sinha";
+  document.getElementById("changedImage").src =
+    "/assets/images/timeline/2019.png";
 }
